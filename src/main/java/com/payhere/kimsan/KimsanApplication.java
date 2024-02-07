784d22754684e6e4454b5a6075f481105ed8770d
tomato <devmtn30@gmail.com>
2024-02-07 16:15:40 +0900

    init

---
+++ src/main/java/com/payhere/kimsan/KimsanApplication.java
@@ -0,0 +1,13 @@
+package com.payhere.kimsan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KimsanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KimsanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/payhere/kimsan/KimsanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payhere.kimsan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KimsanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
